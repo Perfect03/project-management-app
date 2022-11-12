bdbc21a60d0faa3ff9f94439ed321d0d62a4ae0f
alisafed <alisa.fed.91@gmail.com>
2022-11-12 21:05:24 +0300

    fix: delete console log

--- src/components/forms/SubmitButton.tsx
+++ src/components/forms/SubmitButton.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const SubmitButton = () => {
+  const navigate = useNavigate();
   return (
-    <button className="user-form__button" data-testid="buttonForm" type="submit">
+    <button
+      onClick={() => {
+        navigate(`/`);
+      }}
+      className="user-form__button"
+      data-testid="buttonForm"
+      type="submit"
+    >
       SUBMIT
     </button>
   );
