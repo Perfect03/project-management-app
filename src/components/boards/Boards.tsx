aee5dce8374da57ace799639e0cd2123df3123d7
alisafed <alisa.fed.91@gmail.com>
2022-11-14 20:03:35 +0300

    feat: start board page

--- src/components/boards/Boards.tsx
+++ src/components/boards/Boards.tsx
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './boards.scss';
+import { Modal } from './AddBoard';
 
 const Boards = () => {
-  return <div className="error-page"></div>;
+  const [isAddBoard, setAddBoard] = useState(false);
+  return (
+    <>
+      <section className="boards">
+        <ul className="boards-table">
+          <li>
+            <button className="boards-table__add" onClick={() => setAddBoard(true)}></button>
+          </li>
+        </ul>
+      </section>
+      <Modal
+        isVisible={isAddBoard}
+        title="Create new board:"
+        content={
+          <>
+            <input className="board-modal__input" placeholder="Board's name" type="text" />
+            <input className="board-modal__input" placeholder="Discription" type="text" />
+            <input className="board-modal__input" placeholder="Add users" type="text" />
+            <section className="board-modal-box-button">
+              <button className="board-modal__button save">SAVE</button>
+              <button className="board-modal__button back">BACK</button>
+            </section>
+          </>
+        }
+        onClose={() => setAddBoard(false)}
+      />
+    </>
+  );
 };
 
 export { Boards };
