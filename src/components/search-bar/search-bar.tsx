735333d29431211c75623cb843a8b1c9ba4c5d12
AlisaFed <alisa.fed.91@gmail.com>
2022-12-06 02:07:12 +0300

    fix: fix search

--- src/components/search-bar/search-bar.tsx
+++ src/components/search-bar/search-bar.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent, useState } from 'react';
+import React, { FormEvent } from 'react';
 import './search-bar.scss';
 import '../boards/boards.scss';
 import { useDispatch, useSelector } from 'react-redux';
